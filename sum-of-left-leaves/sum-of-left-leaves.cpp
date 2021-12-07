5c38c458ec507853949b0c2a649e91f7f0983a8a
Huy Nguyen <40990575+minhhuy24072002@users.noreply.github.com>
2021-12-06 19:22:51 -0600

    Time: 0 ms (100.00%), Space: 13.4 MB (74.19%) - LeetHub

---
+++ sum-of-left-leaves/sum-of-left-leaves.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        int sum = 0;
+        
+        if (root -> left) {
+            if (!(((root ->left) -> left) || ((root ->left) -> right))) {
+                cout << (root ->left) -> val << endl;
+                sum += (root ->left) -> val;
+            }
+        }
+        
+        if (root -> left) {
+            sum += sumOfLeftLeaves(root -> left) ;
+        } 
+        
+        if (root -> right) {
+            sum += sumOfLeftLeaves(root -> right);
+        }
+        
+        return sum;
+    }
+};
