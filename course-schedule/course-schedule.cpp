08951192528a9ac75df6379723a641ed3450a8b7
Huy Nguyen <40990575+minhhuy24072002@users.noreply.github.com>
2021-12-19 02:02:56 -0600

    Time: 32 ms (22.86%), Space: 15.5 MB (15.27%) - LeetHub

---
+++ course-schedule/course-schedule.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isCyclic(int currCourse, vector <int> adj[], map<int,int> &mark, set<int> &prevCourses) {
+        bool check = false;
+        if (adj[currCourse].size() == 0) return false;
+        mark[currCourse] = 1;
+        prevCourses.insert(currCourse);
+        
+        for (int i = 0; i < adj[currCourse].size(); i++) 
+            if (!mark[adj[currCourse][i]])
+                check |= isCyclic(adj[currCourse][i], adj, mark, prevCourses);
+            else if (prevCourses.count(adj[currCourse][i]) > 0) { 
+                cout << prevCourses.count(adj[currCourse][i]) << ' ' << adj[currCourse][i] << ' ' << currCourse << endl;
+                return true;
+            }
+        prevCourses.erase(currCourse);
+        return check;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector <int> adj[numCourses];
+        map <int, int> mark;
+        for (int i = 0; i < prerequisites.size(); i++) 
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        
+        for (int i = 0; i < numCourses; i++) {
+            set<int> temp;
+            cout << temp.count(552) << endl;
+            if (isCyclic(i, adj, mark, temp)) return false;
+        }
+        
+        return true;
+    }
+};
