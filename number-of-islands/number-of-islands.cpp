860b6ddf27e96f23c09ff96d0fc525420f371b22
Huy Nguyen <40990575+minhhuy24072002@users.noreply.github.com>
2021-12-19 00:06:09 -0600

    Time: 128 ms (5.08%), Space: 25.3 MB (6.13%) - LeetHub

--- number-of-islands/number-of-islands.cpp
+++ number-of-islands/number-of-islands.cpp
@@ -1,38 +1,36 @@
 class Solution {
 public:
     bool check(int x, int y, int dx, int dy, int m, int n) {
-        if (x + dx < 0) return false;
         if (x + dx >= m) return false;
-        if (y + dy < 0) return false;
+        if (x + dx < 0) return false;
         if (y + dy >= n) return false;
+        if (y + dy < 0) return false;
         return true;
     }
     int numIslands(vector<vector<char>>& grid) {
+        map<pair<int, int>, int> mark;
+        int res = 0, x, y;
+        queue <pair<int, int>> gridQueue;
         const int dx[4] = {-1, 0, 0, 1};
         const int dy[4] = {0, -1, 1, 0};
-        int res = 0, x, y;
-        
-        map<pair<int,int>,int> mark;
-        
-        queue<pair<int,int>> qu;
         
         for (int i = 0; i < grid.size(); i++) {
             for (int j = 0; j < grid[0].size(); j++) {
-                if (grid[i][j] == '1' && !mark[make_pair(i,j)]) {
+                if (!mark[{i, j}] && grid[i][j] == '1') {
                     res++;
+                    gridQueue.push({i, j});
                     
-                    qu.push(make_pair(i,j));
-                    while (qu.size()) {
-                        x = qu.front().first;
-                        y = qu.front().second;
-                        mark[make_pair(x,y)] = 1;
-                        qu.pop();
+                    while (gridQueue.size()) {
+                        x = gridQueue.front().first;
+                        y = gridQueue.front().second;
+                        mark[{x, y}] = 1;
+                        gridQueue.pop();
                         
-                        for (int direction = 0; direction < 4; direction++) {
-                            if (check(x, y, dx[direction], dy[direction], grid.size(), grid[0].size())) {
-                                if (grid[x + dx[direction]][y + dy[direction]] == '1' && !mark[make_pair(x + dx[direction], y + dy[direction])]) {
-                                    qu.push(make_pair(x + dx[direction], y + dy[direction]));
-                                    mark[make_pair(x + dx[direction], y + dy[direction])] = 1;
+                        for (int dir = 0; dir < 4; dir++) {
+                            if (check(x, y, dx[dir], dy[dir], grid.size(), grid[0].size())) {
+                                if (grid[x + dx[dir]][y + dy[dir]] == '1' && !mark[{x + dx[dir], y + dy[dir]}]) {
+                                    gridQueue.push({x + dx[dir], y + dy[dir]});
+                                    mark[{x + dx[dir], y + dy[dir]}] = 1;
                                 }
                             }
                         }
@@ -40,6 +38,7 @@ public:
                 }
             }
         }
+        
         return res;
     }
 };
