95870d653aa2dce4557cc0a9dfddfef0fda41620
Huy Nguyen <40990575+minhhuy24072002@users.noreply.github.com>
2021-12-25 13:05:18 -0600

    Time: 108 ms (50.84%), Space: 31.9 MB (13.46%) - LeetHub

--- longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        int count = 0, res = 0, curr;
         map<int, int> mark;
+        int count, res = 0, curr;
         
         for (int i = 0; i < nums.size(); i++) {
             mark[nums[i]] = 1;
         }
         
-        for (int i = 0; i < nums.size(); i++)
-            if (!(mark[nums[i] - 1]) && mark[nums[i]] != 2) {
-                count = 1;
+        for (int i = 0; i < nums.size(); i++) {
+            if (!mark[nums[i] - 1] && mark[nums[i]] != 2) {
+                count = 0;
                 curr = nums[i];
-                while (mark[curr + 1] > 0) {
-                    cout << curr << endl;
+                    
+                while (mark[curr] > 0) {
                     count++;
                     curr++;
                 }
+                cout << nums[i] << ' ' << count << endl;
+                
                 mark[nums[i]] = 2;
                 res = max(res, count);
             }
+        }
         
         return res;
     }
