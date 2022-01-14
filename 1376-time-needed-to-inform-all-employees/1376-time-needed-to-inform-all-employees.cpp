1731f80810b680a78fa7f6d2cbc1d0e6607da2c5
Huy Nguyen <40990575+minhhuy24072002@users.noreply.github.com>
2022-01-14 02:17:17 -0600

    Time: 252 ms (81.77%), Space: 122.3 MB (49.47%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,25 +1,24 @@
 class Solution {
-private:
-    int dfs(vector<vector<int>> &graph, vector<int>& informTime, int managerID) {
-        int res = 0;
+public:
+    int dfs(int currEmployee, vector<int> &informTime,  vector<vector<int>> &adj) {
         
-        for (int &eachEmployee : graph[managerID]) {
-            res = max(res, dfs(graph, informTime, eachEmployee));
+        int maxTime = 0;
+        
+        for (int i : adj[currEmployee]) {
+            maxTime = max(maxTime, dfs(i, informTime, adj));
         }
         
-        return res + informTime[managerID];
+        return maxTime + informTime[currEmployee];
     }
-public:
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-        vector<vector<int>> graph(n); // {manager {employee}}
+        vector<vector<int>> adj(n);
         
         if (n == 1) return 0;
         
-        for (int i = 0; i < n; i++) {
-            if (i == headID) continue;
-            graph[manager[i]].emplace_back(i);
-        }
+        for (int i = 0; i < manager.size(); i++)
+            if (manager[i] > -1)
+                adj[manager[i]].push_back(i);
         
-        return dfs(graph, informTime, headID);
+        return dfs(headID, informTime, adj);
     }
 };
